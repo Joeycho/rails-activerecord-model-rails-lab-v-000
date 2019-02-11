f722f0006fb2aa3e4dcc5c5c8bea45852038f567
Joey Cho <cjy12182000@naver.com>
2019-02-11 13:25:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,12 +1,10 @@
 class CreateStudents < ActiveRecord::Migration
   
   def change
-    create_table :posts do |t|
-      t.string :title
-      t.text :description
- 
-      t.timestamps null: false
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
     end
   end
-end
+
 end
